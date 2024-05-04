43eff481984a65525d8f6eb0b3a2e250c4f70b64
salisKingdoms <aryanisalis095@gmail.com>
2024-05-04 15:46:31 +0700

    add modals delete

--- CVSalis/wwwroot/myjs/CVDetails.js
+++ CVSalis/wwwroot/myjs/CVDetails.js
@@ -1,4 +1,6 @@
 ﻿var table = null;
+var tempExpNew = [];
+var rowTempExpDelete = 0;
 $(document).ready(function () {
     table = $('#listDataExp').DataTable({
         lengthMenu: [],
@@ -46,7 +48,7 @@ function onAddNewExp(obj, id) {
     //});
     $('#listCV').css('display', 'none');
     $('#formCV').css('display', 'block');
-    var totalRow = $("#listDataExp").find("tr:not(:first)").length;
+    var totalRow = table.rows().count();
     var dataExp = {
         indexRow: totalRow+1,
         empID: $('#cvID').val(),
@@ -60,6 +62,7 @@ function onAddNewExp(obj, id) {
     };
 
     console.log(dataExp);
+    tempExpNew.push(dataExp);
     var htmlRow = $("<tr>" + '<td style="display:none; text-align:center;">' + 0 + "</td>" +
         '<td style="text-align: center;">' + '<button class="btn btn-danger me-2"  type="submit"  onclick=OnDeletedExpNew(' + dataExp.indexRow + '); >Delete</button>' + "</td>" +
         '<td style="text-align:center;">' + dataExp.companyName + "</td>" +
@@ -84,12 +87,55 @@ function onAddNewExp(obj, id) {
 }
 
 function OnDeletedExpNew(row) {
-    var originalData = $('#listDataExp').DataTable().rows().data().toArray()
-    $('#listDataExp tbody tr').eq(row).remove();
-    var tables = $('#myDataTable').DataTable();
-    tables.clear().rows.add(originalData).draw();
+    rowTempExpDelete = row;
+    $('#modalExpTemp').css('display', 'block');
+    $('#modalExpTemp').modal("show");
+    
 }
 
+function OnDeleteOK() {
+    
+    $('#modalExpTemp').modal("hide");
+    $('#mySpinner').css('display', 'block');
+
+    tempExpNew = tempExpNew.splice(rowTempExpDelete, 1);
+    console.log("ini data temp:" + tempExpNew);
+    $.each(tempExpNew, function (i, item) {
+        item.indexRow -= 1;
+    });
+
+    rowTempExpDelete = 0;
+    table.clear();
+    $("#listDataExp").find("tr:not(:first)").remove();
+    $('#listDataExp').DataTable().destroy();
+
+    $.each(tempExpNew, function (i, data) {
+        var htmlRow = $("<tr>" + '<td style="display:none; text-align:center;">' + 0 + "</td>" +
+            '<td style="text-align: center;">' + '<button class="btn btn-danger me-2"  type="submit"  onclick=OnDeletedExpNew(' + data.indexRow + '); >Delete</button>' + "</td>" +
+            '<td style="text-align:center;">' + data.companyName + "</td>" +
+            '<td style="text-align:center;">' + data.role + "</td>" +
+            '<td style="text-align:center;">' + data.compAddress + "</td>" +
+            '<td style="text-align:center;">' + data.startYear + "</td>" +
+            '<td style="text-align:center;">' + data.endYear + "</td>" +
+            '<td style="text-align:center;">' + data.tools + "</td>" +
+            '<td style="text-align:center;">' + data.responsibility + "</td></tr>");
+
+        table.row.add($(htmlRow));
+
+    });
+
+    //redraw datatable with new array after splice/deleted
+    table.draw();
+    //hide spinner 3 seconds
+    setTimeout(function () {
+        $('#mySpinner').css('display', 'none');
+    }, 3000);
+}
+
+function OnCloseModal() {
+    $('#modalExpTemp').css('display', 'none');
+    $('#modalExpTemp').modal("hide");
+}
 
 function saveData() {
     //if ($('#cvID').val() == "" || parseInt($('#cvID').val()) == 0) {
